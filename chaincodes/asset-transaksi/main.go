77e587ed2d6fe72f6f64c389d7dab399e429daa3
Rahman Insani <rahmaninsani54@gmail.com>
2023-07-11 13:41:49 +0700

    updating config and chaincodes

---
+++ chaincodes/asset-transaksi/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	chaincode "asset-transaksi/chaincode"
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+	"log"
+)
+
+func main() {
+	assetChaincode, err := contractapi.NewChaincode(&chaincode.Transaction{})
+	if err != nil {
+		log.Panicf("Error creating chaincode: %v", err)
+	}
+
+	if err := assetChaincode.Start(); err != nil {
+		log.Panicf("Error starting chaincode: %v", err)
+	}
+}
