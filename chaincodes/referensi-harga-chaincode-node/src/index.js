f3ddb5925221b666e969dd5a7087d156c37b0763
Rahman Insani <rahmaninsani54@gmail.com>
2023-07-17 09:18:27 +0700

    update chaincode

---
+++ chaincodes/referensi-harga-chaincode-node/src/index.js
@@ -0,0 +1,4 @@
+const ReferensiHargaChaincode = require("./chaincode/ReferensiHargaChaincode.js");
+
+module.exports.ReferensiHargaChaincode = ReferensiHargaChaincode;
+module.exports.contracts = [ReferensiHargaChaincode];
