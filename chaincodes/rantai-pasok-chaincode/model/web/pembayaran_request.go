842236c717db9ac10387f74354cd4272e87b0299
Rahman Insani <rahmaninsani54@gmail.com>
2023-08-05 17:56:19 +0700

    adding pembayaran detail

--- chaincodes/rantai-pasok-chaincode/model/web/pembayaran_request.go
+++ chaincodes/rantai-pasok-chaincode/model/web/pembayaran_request.go
@@ -6,8 +6,12 @@ type PembayaranCreateRequest struct {
 	JenisUser             string `json:"jenisUser"`
 	Nomor                 string `json:"nomor"`
 	Tanggal               string `json:"tanggal"`
+	NamaBankPengirim      string `json:"namaBankPengirim"`
 	NomorRekeningPengirim string `json:"nomorRekeningPengirim"`
+	NamaPengirim          string `json:"namaPengirim"`
+	NamaBankPenerima      string `json:"namaBankPenerima"`
 	NomorRekeningPenerima string `json:"nomorRekeningPenerima"`
+	NamaPenerima          string `json:"namaPenerima"`
 	CidBuktiPembayaran    string `json:"cidBuktiPembayaran"`
 	CreatedAt             string `json:"createdAt"`
 	UpdatedAt             string `json:"updatedAt"`
